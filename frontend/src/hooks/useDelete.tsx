070bdc885d0278671b5a7a3d5e4c2782c3a57a7e
Francis Akabuogu <fakabuogu@gmail.com>
2022-06-08 12:04:31 +0000

    DELETE to POST

--- frontend/src/hooks/useDelete.tsx
+++ frontend/src/hooks/useDelete.tsx
@@ -8,7 +8,7 @@ export const useDelete = () => {
 
   const deleteProducts = async (skus: string[]) => {
     const response = await fetch(baseUrl + '/delete', {
-      method: 'DELETE',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
